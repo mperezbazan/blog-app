c5978cf327ac0b48c106a2e14794f1266169bd0d
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-21 19:17:33 -0300

    update posts migration

--- db/migrate/20230221221546_create_posts.rb
+++ db/migrate/20230221221546_create_posts.rb
@@ -2,8 +2,10 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.integer :author_id
-      t.integer :post_id
+      t.string :title
       t.text :text
+      t.integer :comments_counter
+      t.integer :likes_counter
 
       t.timestamps
     end
