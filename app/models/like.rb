fd4e415507282f10084f4e6f91680e4f505eb2e5
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-22 12:54:00 -0300

    Add method to increment likes to post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  after_create :increment_likes
+
+  def increment_likes
+    post.increment!(:likes_counter)
+    post.save
+    post.reload
+  end
 end
