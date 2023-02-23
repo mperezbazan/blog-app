95833e41fc88027cfa8453cd0885ccba3a72f0a4
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-23 13:44:03 -0300

    Add test for user post likes comments

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,16 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0) }
+  before { subject.save }
   it 'name should be present' do
-    user = User.new(name: nil)
-    expect(user).to_not be_valid
+    subject.name = nil
+    expect(subject).to_not be_valid
   end
-  it 'posts_counter must be integer >= 0' do
-    user = User.new(name: 'Name', posts_counter: 3.2)
-    expect(user).to_not be_valid
+  it 'posts_counter must be integer' do
+    subject.posts_counter = 1.2
+    expect(subject).to_not be_valid
   end
-  it 'posts_counter is valid with 0' do
-    user = User.new(name: 'Name', posts_counter: 0)
-    expect(user).to be_valid
+  it 'posts_counter must to be >= 0' do
+    subject.posts_counter = -1
+    expect(subject).to_not be_valid
+  end
+  it 'most recent post must be array' do
+    expect(subject.recent_posts.length).to eql(0)
   end
 end
