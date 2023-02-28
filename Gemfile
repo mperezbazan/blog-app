69080bdf70d186a3e99449c2d848423d150dcc60
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-28 14:12:10 -0300

    Add test files for posts and users

--- Gemfile
+++ Gemfile
@@ -74,5 +74,6 @@ end
 gem 'rubocop', '>= 1.0', '< 2.0'
 
 group :development, :test do
+  gem 'rails-controller-testing'
   gem 'rspec-rails'
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  test_user = User.find(1)
+  test_post = Post.create(
+    author_id: test_user.id,
+    title: 'Hello',
+    text: 'This is my first post',
+    comments_counter: 0,
+    likes_counter: 0
+  )
   describe 'GET /index' do
     it 'returns http success' do
-      get '/posts/index'
+      get "/users/#{test_user.id}/posts"
       expect(response).to have_http_status(:success)
+    end
+    it 'render the index template wit Posts list text' do
+      get "/users/#{test_user.id}/posts"
+      expect(response.body).to include('Posts list')
+    end
+    it 'render the index template' do
+      get "/users/#{test_user.id}/posts"
+      expect(response).to render_template('posts/index')
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/posts/show'
+      get "/users/#{test_user.id}/posts/#{test_post.id}"
       expect(response).to have_http_status(:success)
+    end
+    it 'render the show template with Post details text' do
+      get "/users/#{test_user.id}/posts/#{test_post.id}"
+      expect(response.body).to include('Post detail')
+    end
+    it 'render the show template' do
+      get "/users/#{test_user.id}/posts/#{test_post.id}"
+      expect(response).to render_template('posts/show')
     end
   end
 end
